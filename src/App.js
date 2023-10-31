a5438fc8de8f0d204bbb916a7457b57fe4a2e25c
Adriano Martiliano <adrianomartiliano@gmail.com>
2023-10-31 06:00:49 -0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+/* CSS */
 import './App.css';
+
+//Componentes
+import FirstComponent from './components/FirstComponent.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fundamentos - Componentes</h1>
+      <FirstComponent />
     </div>
+    
   );
 }
 
